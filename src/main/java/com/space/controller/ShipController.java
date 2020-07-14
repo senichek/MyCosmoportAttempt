544f0a77931d7f906efbb26c0020283865ece5b9
Senichek <senichek@gmail.com>
2020-07-14 18:36:46 +0200

    List of Ships gets populated

---
+++ src/main/java/com/space/controller/ShipController.java
@@ -0,0 +1,30 @@
+package com.space.controller;
+
+import com.space.model.Ship;
+import com.space.service.ShipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ShipController {
+
+    private ShipService shipService;
+
+    public ShipController() {
+    }
+
+    @Autowired
+    public ShipController(ShipService shipService) {
+        this.shipService = shipService;
+    }
+
+    @RequestMapping(path = "/rest/ships", method = RequestMethod.GET)
+    public List<Ship> getAllShips() {
+
+        return shipService.getAllShipsNoParam();
+    }
+}
---
+++ src/main/java/com/space/service/ShipServiceImpl.java
@@ -0,0 +1,84 @@
+package com.space.service;
+
+import com.space.controller.ShipOrder;
+import com.space.model.Ship;
+import com.space.model.ShipType;
+import com.space.repository.ShipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Transactional
+public class ShipServiceImpl implements ShipService {
+
+    private ShipRepository shipRepository;
+
+
+    public ShipServiceImpl() {
+    }
+
+    @Autowired
+    public ShipServiceImpl(ShipRepository shipRepository) {
+        //super();
+        this.shipRepository = shipRepository;
+    }
+
+    @Override
+    public List<Ship> getAllShipsNoParam() {
+        List<Ship> allShipsList = new ArrayList<>();
+        Iterable<Ship> ships = shipRepository.findAll();
+        for (Ship x : ships) {
+            allShipsList.add(x);
+        }return allShipsList;
+    }
+
+    @Override
+    public Ship saveShip(Ship ship) {
+        return null;
+    }
+
+    @Override
+    public Ship getShip(Long id) {
+        return null;
+    }
+
+    @Override
+    public Ship updateShip(Ship oldShip, Ship newShip) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public void deleteShip(Ship ship) {
+
+    }
+
+    @Override
+    public List<Ship> getShips(String name, String planet, ShipType shipType, Long after, Long before, Boolean isUsed, Double minSpeed, Double maxSpeed, Integer minCrewSize, Integer maxCrewSize, Double minRating, Double maxRating) {
+        return null;
+    }
+
+    @Override
+    public List<Ship> sortShips(List<Ship> ships, ShipOrder order) {
+        return null;
+    }
+
+    @Override
+    public List<Ship> getPage(List<Ship> ships, Integer pageNumber, Integer pageSize) {
+        return null;
+    }
+
+    @Override
+    public boolean isShipValid(Ship ship) {
+        return false;
+    }
+
+    @Override
+    public double computeRating(double speed, boolean isUsed, Date prod) {
+        return 0;
+    }
+}
